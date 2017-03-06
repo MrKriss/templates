57a99270fe64635ae8c69c9c8c70fed343b2dcc2
Chris Musselle <chris.j.musselle@gmail.com>
2017-03-06 22:22:08 +0000

    Add env creation script

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,37 @@
+""" Hooks to create a new conda environment with the latest version of common libraries 
+
+Also optionally create a git repo and commit the project contents
+"""
+
+import subprocess
+import shlex
+
+# The string in these variables will be overriden by cookiecutter
+PROJECT_NAME = "{{cookiecutter.project_name|lower|replace(' ', '_')}}"
+PYTHON_VERSION = "{{cookiecutter.python_version}}"
+PYTHON_LIBRARIES = "{{cookiecutter.python_libraries}}"
+
+# Create the strings for commands to run, substituting with the values from cookiecutter
+conda_install_cmd = f"conda create -y -n {PROJECT_NAME} python={PYTHON_VERSION} {PYTHON_LIBRARIES}"
+
+conda_save_env_cmd = f"conda env export -n {PROJECT_NAME} -f environment.yml"
+
+# Execute command with subprocess module
+print(f'Installing conda environment for {PROJECT_NAME} ...' )
+subprocess.run(shlex.split(conda_install_cmd))
+print(f'Finished installing conda environment for {PROJECT_NAME}')
+
+print('Saving snapshot of conda environment to "environment.yml" ... ', end='')
+subprocess.run(shlex.split(conda_save_env_cmd))
+print('Complete!')
+
+
+# If using git, initialise the rep.
+# The code in this following Jinja2 block only gets inserted when "git_usage" is set to Yes
+{% if cookiecutter.git_usage == "Yes" %}
+print(f'Initialising Git repo and creating initial commit for {PROJECT_NAME} ...')
+subprocess.run(shlex.split('git init'))
+subprocess.run(shlex.split('git add *'))
+subprocess.run(shlex.split('git commit -m "Initial commit of project template"'))
+print("Complete!!!")
+{% endif %}
